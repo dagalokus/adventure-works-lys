4e56fc317efeb170e34a4f2d0ecd80e8001845d0
felipe.sanchezplazas@gmail.com <felipe.sanchezplazas@gmail.com>
2025-11-07 22:51:17 +0000

    Added formatting to the 4 stg models we created

--- models/stg/stg_department.sql
+++ models/stg/stg_department.sql
@@ -1,10 +1,8 @@
-with source_data as (
-    select
-        department_id,
-        name,
-        group_name,
-        modified_date as modified_date_at
-    from {{ source('HR', 'department') }}
-)
+with
+    source_data as (
+        select department_id, name, group_name, modified_date as modified_date_at
+        from {{ source("HR", "department") }}
+    )
 
-select * from source_data
+select *
+from source_data
