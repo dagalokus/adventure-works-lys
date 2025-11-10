9fb079bbc864ff8373e5e050685e8718d2a1c7c9
felipe.sanchezplazas@gmail.com <felipe.sanchezplazas@gmail.com>
2025-11-10 23:07:21 +0000

    Added some adjustments in the Int_employee_department model to only view the actual employees of the company. Formated the code on the stg_person model

--- models/stg/stg_person.sql
+++ models/stg/stg_person.sql
@@ -1,9 +1,7 @@
-with source_data as (
-    select
-        business_entity_id,
-        person_type,
-        first_name,
-        last_name
-    from {{ source('Person', 'person') }}
-)
-select * from source_data
+with
+    source_data as (
+        select business_entity_id, person_type, first_name, last_name
+        from {{ source("Person", "person") }}
+    )
+select *
+from source_data
