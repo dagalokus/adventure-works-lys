55f333898fb8ee879e677eb737a0ef65ede6998a
felipe.sanchezplazas@gmail.com <felipe.sanchezplazas@gmail.com>
2025-11-10 21:35:40 +0000

    Added some missing info on the project YAML file. Added some indentation missing on the testing under the sources YAML file

---
+++ models/stg/stg_person.sql
@@ -0,0 +1,9 @@
+with source_data as (
+    select
+        business_entity_id,
+        person_type,
+        first_name,
+        last_name
+    from {{ source('Person', 'person') }}
+)
+select * from source_data
