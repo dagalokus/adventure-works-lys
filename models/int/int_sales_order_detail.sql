80194dea0f40be17125d88b1db6a363e88ef7fd0
felipe.sanchezplazas@gmail.com <felipe.sanchezplazas@gmail.com>
2025-11-14 01:02:05 +0000

    Fixed the order_date column on the int model for sales_order_detail model

--- models/int/int_sales_order_detail.sql
+++ models/int/int_sales_order_detail.sql
@@ -3,12 +3,13 @@ with
         select so.*,sh.order_date
         from {{ ref("stg_sales_order_detail") }} as so
         left join {{ ref('stg_sales_order_header') }} as sh
+        on so.sales_order_id = sh.sales_order_id
     ),
 
     final as (
         select
             sales_order_id,
-            sales_order_detail_id
+            sales_order_detail_id,
             order_date,
             order_qty,
             product_id,
