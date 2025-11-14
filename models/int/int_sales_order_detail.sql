16372ac17f99822392400eb9e0b46974f40c7939
felipe.sanchezplazas@gmail.com <felipe.sanchezplazas@gmail.com>
2025-11-14 00:47:58 +0000

    Refactor the sales_order_header into sales_order_detail as we change our fact table in our modelling. Now our grain is going to be using sales_order_detail_id to see more information.

---
+++ models/int/int_sales_order_detail.sql
@@ -0,0 +1,22 @@
+with
+    sales_order_detail as (
+        select so.*,sh.order_date
+        from {{ ref("stg_sales_order_detail") }} as so
+        left join {{ ref('stg_sales_order_header') }} as sh
+    ),
+
+    final as (
+        select
+            sales_order_id,
+            sales_order_detail_id
+            order_date,
+            order_qty,
+            product_id,
+            unit_price,
+            unit_price_discount,
+            line_total as total
+        from sales_order_detail
+    )
+
+select *
+from final
--- models/mart/fact_sales.sql
+++ models/mart/fact_sales.sql
@@ -1,4 +1,4 @@
-with fact_sales as (select * from {{ ref("int_sales_order_header") }})
+with fact_sales as (select * from {{ ref("int_sales_order_detail") }})
 
 select *
 from fact_sales
