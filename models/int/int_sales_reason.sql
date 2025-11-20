cec747cad702afd1e1193038483ef5a1ff1af1ed
FelipeSQLCrack <felipe.sanchezplazas@gmail.com>
2025-11-20 14:27:37 -0500

    Fixed the import CTE's at the beggining of the code to follow dbt best practices

--- models/int/int_sales_reason.sql
+++ models/int/int_sales_reason.sql
@@ -1,15 +1,21 @@
 with
     sales_reason as (
         select *
-        from {{ ref("stg_sales_reason") }}
-        inner join
-            {{ ref("stg_sales_order_header_sales_reason") }} using (sales_reason_id)
+        from {{ ref("stg_sales_reason") }}),
+
+    sales_order_header_sales_reason as (
+        select *
+        from {{ ref("stg_sales_order_header_sales_reason") }}
 
     ),
 
     final as (
-        select sales_order_id, sales_reason_id, name, reason_type from sales_reason
+        select so.sales_order_id, so.sales_reason_id, s.name, s.reason_type
+        from sales_order_header_sales_reason as so
+        inner join sales_reason as s
+        on so.sales_reason_id = s.sales_reason_id
     )
 
 select *
 from final
+
