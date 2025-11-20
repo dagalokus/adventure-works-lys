05130d76db4c5e0e60b736ec73fa2338965c0989
FelipeSQLCrack <felipe.sanchezplazas@gmail.com>
2025-11-20 14:16:02 -0500

    Change the Import CTE structure on the sales_order_detail model to follow dbt best practices

--- models/int/int_sales_order_detail.sql
+++ models/int/int_sales_order_detail.sql
@@ -1,24 +1,35 @@
 with
-    sales_order_detail as (
-        select so.*, sh.order_date
-        from {{ ref("stg_sales_order_detail") }} as so
-        left join
-            {{ ref("stg_sales_order_header") }} as sh
-            on so.sales_order_id = sh.sales_order_id
-    ),
+  sales_order_detail_source as (
+    select *
+    from {{ ref("stg_sales_order_detail") }}
+  ),
 
-    final as (
-        select
-            sales_order_id,
-            sales_order_detail_id,
-            order_date,
-            order_qty,
-            product_id,
-            unit_price,
-            unit_price_discount,
-            line_total as total
-        from sales_order_detail
-    )
+  sales_order_header_source as (
+    select
+      sales_order_id,
+      order_date
+    from {{ ref("stg_sales_order_header") }}
+  ),
+
+  sales_order_detail as (
+    select
+      so.sales_order_id,
+      so.sales_order_detail_id,
+      sh.order_date,
+      so.order_qty,
+      so.product_id,
+      so.unit_price,
+      so.unit_price_discount,
+      so.line_total as total
+    from sales_order_detail_source as so
+    left join sales_order_header_source as sh
+      on so.sales_order_id = sh.sales_order_id
+  ),
+
+  final as (
+    select *
+    from sales_order_detail
+  )
 
 select *
 from final
